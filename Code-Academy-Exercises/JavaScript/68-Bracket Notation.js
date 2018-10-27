a393601b64d9a727973bc4a56df247f68d111af6
Jose Quevedo <jlquevedo88@gmail.com>
2018-10-27 13:09:09 -0600

    Objects

---
+++ Code-Academy-Exercises/JavaScript/68-Bracket Notation.js
@@ -0,0 +1,12 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+
+// Write your code below
+const isActive = spaceship['Active Mission']
+console.log(spaceship['Active Mission']);
